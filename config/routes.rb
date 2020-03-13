be616b47b3211b91f45703c4821a7e29c0f76920
jpk4 <jkleysen@academic.rrc.ca>
2020-03-13 04:39:57 -0500

    Edit permalinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :pages
   root to: 'houses#index'
 
   resources 'houses', only: %i[index show]
@@ -10,6 +9,9 @@ Rails.application.routes.draw do
   resources 'aliases', only: %i[index show]
   resources 'books', only: %i[index show]
   resources 'titles', only: %i[index show]
+  resources :pages
+
+  get ':permalink', to: 'pages#permalink', as: 'permalink'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
