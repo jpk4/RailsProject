eca40003504de7ca0ac3b03b91e97218638c2a74
jpk4 <jkleysen@academic.rrc.ca>
2020-03-13 00:38:10 -0500

    Created Character controller, view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'seats/index'
-  get 'seats/show'
+  get 'characters/index'
+  get 'characters/show'
   root to: 'houses#index'
 
   resources 'houses', only: %i[index show]
+  resources 'seats', only: %i[index show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
