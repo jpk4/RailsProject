0d8bf2d1eff6e77af9ad4a3d125a91507a4f24b2
jpk4 <jkleysen@academic.rrc.ca>
2020-03-13 00:37:48 -0500

    Updated views, routes and seat controller

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class SeatsController < ApplicationController
   def index
+    @seats = Seat.includes(:house).order(:name)
   end
 
   def show
+    @seat = Seat.find(params[:id])
   end
 end
