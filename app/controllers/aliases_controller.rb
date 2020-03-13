9fb226c4c8b18fb7e19441b4e1af74328fc51cf0
jpk4 <jkleysen@academic.rrc.ca>
2020-03-13 02:08:38 -0500

    Update Aliases controller, views and routes

--- app/controllers/aliases_controller.rb
+++ app/controllers/aliases_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class AliasesController < ApplicationController
   def index
+    @aliases = Alias.includes(:character).order(:name)
   end
 
   def show
+    @alias = Alias.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'aliases/index'
-  get 'aliases/show'
   root to: 'houses#index'
 
   resources 'houses', only: %i[index show]
   resources 'seats', only: %i[index show]
   resources 'characters', only: %i[index show]
+  resources 'aliases', only: %i[index show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
