140e1eaa29acc7735bbd368d865fab64001a149f
jpk4 <jkleysen@academic.rrc.ca>
2020-03-12 23:47:25 -0500

    Updated index and show views for house controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class HousesController < ApplicationController
   def index
     @houses = House.all
   end
 
   def show
+    @house = House.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root to: 'houses#index'
-  get 'houses/:id', to: 'houses#show', id: /\d+/
+
+  resources 'houses', only: %i[index show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
