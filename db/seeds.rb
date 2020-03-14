2e1d65e1dbac6c3f58ba5a772a4d2e01d236b339
jpk4 <jkleysen@academic.rrc.ca>
2020-03-14 16:23:47 -0500

    Clean up seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
+require 'open-uri'
+require 'json'
+require 'csv'
+require 'resolv-replace'
+
 Seat.destroy_all
 Alias.destroy_all
 Book.destroy_all
 Title.destroy_all
 House.destroy_all
 Character.destroy_all
-
-# frozen_string_literal: true
-
-require 'open-uri'
-require 'json'
-require 'csv'
-require 'resolv-replace'
 
 csv_file = Rails.root + 'db/deaths.csv'
 deaths = CSV.parse(File.read(csv_file), headers: true)
@@ -42,50 +40,6 @@ def character_name_list(characters)
   character_list.join(', ')
 end
 
-character_ids = random_numbers(200, 2138)
-
-character_ids.each do |character_id|
-  character = got_fetch(character_url(character_id))
-  url_id = character['url']
-  name = character['name']
-  born = character['born']
-  died = character['died']
-  culture = character['culture']
-  character_quote = Faker::TvShows::GameOfThrones.quote
-  kill_count = 0
-
-  deaths.each do |death|
-    kill_count += 1 if death['killer'] == name
-  end
-
-  # puts "Name: #{name} - Kill Count: #{kill_count}" if kill_count > 0
-  allegiances = character['allegiances'].map { |house_url| got_fetch(house_url) }
-
-  characters = Character.where(url_id: url_id).first_or_create(name: name, born: born, died: died, culture: culture, quote: character_quote, kill_count: kill_count)
-
-  allegiances.each do |allegiance|
-    characters.houses << House.where(url_id: allegiance['url']).first_or_create(name: allegiance['name'], region: allegiance['region'], coat_of_arms: allegiance['coatOfArms'], words: allegiance['words'])
-  end
-
-  aliases = character['aliases']
-
-  # puts "Character Name: #{name}"
-
-  aliases.each do |aka|
-    next if aka.empty?
-
-    characters.aliases.find_or_create_by(name: aka)
-  end
-
-  titles = character['titles']
-
-  titles.each do |title|
-    next if title.empty?
-
-    characters.titles << Title.where(name: title).first_or_create
-  end
-end
-
 book_ids = 1..12
 
 book_ids.each do |book_id|
@@ -104,6 +58,48 @@ book_ids.each do |book_id|
   end
 end
 
+character_ids = random_numbers(200, 2138)
+
+character_ids.each do |character_id|
+  character = got_fetch(character_url(character_id))
+  url_id = character['url']
+  name = character['name']
+  born = character['born']
+  died = character['died']
+  culture = character['culture']
+  character_quote = Faker::TvShows::GameOfThrones.quote
+  kill_count = 0
+  allegiances = character['allegiances'].map { |house_url| got_fetch(house_url) }
+
+  deaths.each do |death|
+    kill_count += 1 if death['killer'] == name
+  end
+
+  name = 'Unknown' if name.empty?
+
+  characters = Character.where(url_id: url_id).first_or_create(name: name, born: born, died: died, culture: culture, quote: character_quote, kill_count: kill_count)
+
+  allegiances.each do |allegiance|
+    characters.houses << House.where(url_id: allegiance['url']).first_or_create(name: allegiance['name'], region: allegiance['region'], coat_of_arms: allegiance['coatOfArms'], words: allegiance['words'])
+  end
+
+  aliases = character['aliases']
+
+  aliases.each do |aka|
+    next if aka.empty?
+
+    characters.aliases.find_or_create_by(name: aka)
+  end
+
+  titles = character['titles']
+
+  titles.each do |title|
+    next if title.empty?
+
+    characters.titles << Title.where(name: title).first_or_create
+  end
+end
+
 house_ids = random_numbers(10, 444)
 
 house_ids.each do |house_id|
@@ -113,7 +109,6 @@ house_ids.each do |house_id|
   region = house['region']
   coat_of_arms = house['coatOfArms']
   words = house['words']
-
   members = house['swornMembers'].map { |character_url| got_fetch(character_url) }
 
   # puts "Sworn Members: #{members.count}"
@@ -126,12 +121,9 @@ house_ids.each do |house_id|
 
   seats = house['seats']
 
-  # puts "House Name: #{name}"
-
   seats.each do |seat|
     next if seat.empty?
 
-    # puts "Seat: #{seat}"
     houses.seats.find_or_create_by(name: seat)
   end
 
@@ -140,10 +132,10 @@ house_ids.each do |house_id|
   titles.each do |title|
     next if title.empty?
 
-    # puts "Title: #{title}"
     houses.titles << Title.where(name: title).first_or_create
   end
 end
+
 puts "Created #{Book.count} Books."
 puts "Created #{Character.count} Characters."
 puts "Created #{House.count} Houses."
